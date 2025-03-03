179a4970634c5dd3a99f45313713d2a4d9e8232d
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-03-03 21:52:01 +0100

    ejercicio 4

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/main.py
@@ -0,0 +1,19 @@
+# https://www.python.org/
+'''
+Esto es un comentario multilinea
+'''
+
+"""
+Esto tambien es un comentario multilinea
+"""
+
+variable = 0
+
+CONSTANTE = 3.14
+
+int = 0
+float = 1.5
+string = "hola"
+bool = True
+
+print("¡Hola, Python!")
---
+++ 04 - CADENAS DE CARACTERES/main.py
@@ -0,0 +1,46 @@
+"""
+Operaciones
+"""
+
+s1 = "Hola"
+s2 = "Python"
+
+# Concatenacion
+print(s1 + " " + s2)
+
+# Repeticion
+print(s1 * 3)
+
+# Indexacion
+print(s1[0] + s1[1] + s1[2] + s1[3])
+
+# Longitud
+print(len(s1))
+
+# Slicing (porcion)
+print(s2[2:6])
+print(s2[2:])
+print(s2[0:2])
+print(s2[:2])
+
+# Busqueda
+
+
+
+
+
+
+def analiza(palabra1: str, palabra2: str):
+    palabra1 = palabra1.lower()
+    palabra2 = palabra2.lower()
+    if palabra1 == palindromo(palabra2):
+        return("Es un palindromo")
+    pass
+
+def palindromo(palabra):
+    palabra_convertida = palabra[::-1]
+    return palabra_convertida
+
+
+
+print(analiza("arroz" , "zorrA"))
