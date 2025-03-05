adeeaebeccdbd8b4a8d45ae377df93ac93410349
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-03-05 09:24:58 +0100

    modificando archivo 4 errores flake8

--- 04 - CADENAS DE CARACTERES/main.py
+++ 04 - CADENAS DE CARACTERES/main.py
@@ -26,21 +26,16 @@ print(s2[:2])
 # Busqueda
 
 
-
-
-
-
 def analiza(palabra1: str, palabra2: str):
     palabra1 = palabra1.lower()
     palabra2 = palabra2.lower()
     if palabra1 == palindromo(palabra2):
-        return("Es un palindromo")
-    pass
+        return ("Es un palindromo")
+    
 
 def palindromo(palabra):
     palabra_convertida = palabra[::-1]
     return palabra_convertida
 
 
-
-print(analiza("arroz" , "zorrA"))
+print(analiza("arroz", "zorrA"))
