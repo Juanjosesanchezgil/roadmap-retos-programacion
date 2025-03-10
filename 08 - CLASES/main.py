353ef0319f8c4efcf8064a8c57e38d05d6b99f3e
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-03-10 15:04:20 +0100

    start 8

--- 08 - CLASES/main.py
+++ 08 - CLASES/main.py
@@ -1,10 +1,50 @@
+"""
+Ejercicio
+"""
+
+
 class Programmer:
 
-    def __init__(self):
-        pass
+    surname: str = None
+
+    def __init__(self, name: str, age: int, languages: list):
+        self.name = name
+        self.age = age
+        self.languages = languages
 
     def print(self):
-        pass
+        print(
+            f"Nombre: {self.name} | Apellidos: {self.surname} | Edad: {self.age} | Lenguajes: {self.languages}"
+        )
 
 
-ejemplo = Ejemplo()
+my_programmer = Programmer("Brais", 36, ["Python", "Kotlin", "Swift"])
+my_programmer.print()
+my_programmer.surname = "Moure"
+my_programmer.print()
+my_programmer.age = 37
+my_programmer.print()
+
+"""
+Extra
+"""
+
+# LIFO
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.count() == 0:
+            return None
+        return self.stack.pop()
+
+    def count(self):
+        return len(self.stack)
+
