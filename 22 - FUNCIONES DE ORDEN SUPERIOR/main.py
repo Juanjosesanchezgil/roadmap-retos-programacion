b58059d8beed9e357a865273852d2fe5b4d3c879
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-03-24 09:53:40 +0100

    cont 22

--- 22 - FUNCIONES DE ORDEN SUPERIOR/main.py
+++ 22 - FUNCIONES DE ORDEN SUPERIOR/main.py
@@ -10,13 +10,20 @@ numeros = [1, 2, 3, 4, 5]
 print(apply_to_each(square, numeros))
 
 
-estudiantes = [
+students = [
     {"name": "Juan", "birthdate": "23-05-1984", "grades": [5, 7, 8, 9, 10]},
     {"name": "Jose", "birthdate": "23-07-2000", "grades": [4, 7, 6, 9, 3]},
     {"name": "Paco", "birthdate": "23-05-1999", "grades": [5, 7, 6, 7, 2]}
 ]
 
+
 def average(grades):
-    return(sum(grades) / len(grades))
-    
-average(estudiantes["juan"])
+    return (sum(grades) / len(grades))
+
+
+print(
+    list(map(lambda student: {
+        "name": student["name"],
+        "average": average(student["grades"])}, students)
+    )
+)
