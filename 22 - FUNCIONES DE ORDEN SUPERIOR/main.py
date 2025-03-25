e87b979fbbadceaceebcf691ac500d1db85ac56a
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-03-25 14:09:42 +0100

    empezando 23

--- 22 - FUNCIONES DE ORDEN SUPERIOR/main.py
+++ 22 - FUNCIONES DE ORDEN SUPERIOR/main.py
@@ -63,3 +63,49 @@ def sum_values(x, y):
 
 
 print(reduce(sum_values, numbers))
+
+"""
+Extra
+"""
+
+students = [
+    {"name": "Brais", "birthdate": "29-04-1987", "grades": [5, 8.5, 3, 10]},
+    {"name": "moure", "birthdate": "04-08-1995", "grades": [1, 9.5, 2, 4]},
+    {"name": "mouredev", "birthdate": "15-12-2000", "grades": [4, 6.5, 5, 2]},
+    {"name": "supermouredev", "birthdate": "25-01-1980",
+        "grades": [10, 9, 9.7, 9.9]}
+]
+
+
+def average(grades):
+    return sum(grades) / len(grades)
+
+# Promedio
+
+
+print(
+    list(map(lambda student: {
+        "name": student["name"],
+        "average": average(student["grades"])}, students)
+    )
+)
+
+# Mejores
+
+print(
+    list(
+        map(lambda student:
+            student["name"],
+            filter(lambda student: average(student["grades"]) >= 9, students)
+            )
+    )
+)
+
+# Fecha de nacimiento ordenada
+
+print(sorted(students, key=lambda student: datetime.strptime(
+    student["birthdate"], "%d-%m-%Y"), reverse=True))
+
+# Califiación más alta
+
+print(max(map(lambda student: max(student["grades"]), students)))
